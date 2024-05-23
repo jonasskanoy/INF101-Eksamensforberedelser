9809bb633a7d421c24888e911adcd396ae2a5d89
Jonas Berg Skånøy <sm.j@tutamail.com>
2024-05-23 11:25:33 +0200

    Changed fridge class to include my code

--- Eksamen2024/src/Fridge/Fridge.java
+++ Eksamen2024/src/Fridge/Fridge.java
@@ -4,70 +4,71 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-
 public class Fridge implements IFridge {
-    
-    private List<FridgeItem> items;
-    private int numItems;
-    private int maxCapacity;
 
-    public Fridge(int maxCapacity) {
-        this.items = new ArrayList<>();
-        this.numItems = 0;
-        this.maxCapacity = maxCapacity;
+  //TODO: Implementer instansvariabler og konstruktør
+  private ArrayList<FridgeItem> itemsInFridge;
+  private final int MAX_SIZE;
+  public int counterOfItemsInFridge;
+
+  public Fridge(int maxSize) {
+    this.MAX_SIZE = maxSize;
+    this.itemsInFridge = new ArrayList<>();
+    this.counterOfItemsInFridge = 0;
+  }
+
+  @Override
+  public int nItemsInFridge() {
+    return this.counterOfItemsInFridge;
+  }
+
+  @Override
+  public int totalSize() {
+    return this.MAX_SIZE;
+  }
+
+  @Override
+  public boolean placeIn(FridgeItem item) {
+    if (nItemsInFridge() < totalSize()) {
+      this.itemsInFridge.add(item);
+      this.counterOfItemsInFridge++;
+      return true;
+    } else {
+      return false;
     }
-    
-    @Override	
-    public int nItemsInFridge() {
-        return this.numItems;
+  }
+
+  @Override
+  public void takeOut(FridgeItem item) {
+    if (!(this.itemsInFridge.contains(item))) {
+      throw new NoSuchElementException("No such item in the fridge.");
+    } else {
+      this.itemsInFridge.remove(item);
+      this.counterOfItemsInFridge--;
     }
+  }
 
-    @Override
-	public int totalSize() {
-	    return this.maxCapacity;
-	}
-
-	@Override
-	public boolean placeIn(FridgeItem item) {
-        if (items.size() < maxCapacity  && (!item.hasExpired())) {
-            items.add(item);
-            numItems++;
-            return true;
-        }
-        else {
-            return false;
-        }
-	    
-	}
-
-	@Override
-	public void takeOut(FridgeItem item) {
-	    for (int i = items.size() - 1; i > 0; i--) {
-            if (item.equals(items.get(i))) {
-                items.remove(items.get(i));
-                numItems--;
-            }
-            else {
-                throw new NoSuchElementException();
-            }
-        }
-        
+  @Override
+  public void emptyFridge() {
+    if (this.itemsInFridge.isEmpty()) {
+      return;
+    } else {
+      this.counterOfItemsInFridge = 0;
+      this.itemsInFridge.clear();
     }
+  }
 
-	@Override
-	public void emptyFridge() {
-	    this.items.clear();
-	}
-
-	@Override
-	public List<FridgeItem> removeExpiredFood() {
-	    for (int i = items.size() - 1; i > 0; i--) {
-            if (items.get(i).hasExpired()) {
-                items.remove(i);
-                numItems--;
-            }
-        }
-
-        return items;
-	}
+  @Override
+  public List<FridgeItem> removeExpiredFood() {
+    List<FridgeItem> fridgeRemovedExpiredFood = new ArrayList<>(
+      this.itemsInFridge
+    );
+    for (FridgeItem item : this.itemsInFridge) {
+      if (item.hasExpired()) {
+        fridgeRemovedExpiredFood.remove(item);
+        this.counterOfItemsInFridge--;
+      }
+    }
+    return fridgeRemovedExpiredFood;
+  }
 }
